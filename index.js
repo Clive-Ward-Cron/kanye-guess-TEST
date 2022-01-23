04e86e9899b3c74d7b58764d8d1ffee5e5e9064f
Clive Ward-Cron <clive_wardcron@live.com>
2022-01-22 22:58:23 -0700

    Fetching Zen with cors-backdoor, getting Ye from local file. Saving to sessionStorage

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const zendpoint = "https://zenquotes.io/api/quotes/";
-const yendpoint = "https://api.kanye.rest";
+import yeQuotes from './data/kanye';
 
-async function fetchZen() {}
+const zendpoint = 'http://zenquotes.io/api/quotes/';
+// const yendpoint = 'https://api.kanye.rest';
 
-async function fetchYe() {
-  const res = await fetch(yendpoint);
-  const { quote } = await res.json();
-  return quote;
+async function fetchZen() {
+  console.log('Fetching Zen...');
+  const res = await fetch(`http://localhost:1234/`);
+  const data = await res.json();
+  const saved = { data, t: Date.now() };
+  sessionStorage.setItem('zen', JSON.stringify(saved));
+  return data;
 }
 
-console.log(fetchYe());
+async function init() {
+  const z = JSON.parse(sessionStorage.getItem('zen'));
+  const y = JSON.parse(sessionStorage.getItem('ye'));
+
+  const ye = y
+    ? y
+    : !sessionStorage.setItem('ye', JSON.stringify(yeQuotes)) && yeQuotes;
+  const zen = z && Date.now() - z.t < 30000 ? z.data : await fetchZen();
+  console.log(ye);
+  console.log(zen);
+}
+
+init();
